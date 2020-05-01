c606f18da1b3a4fcb404b3c57f966ae02230e9bc
pchong5 <paulchong0613@msn.com>
2020-05-01 05:15:47 -0600

    This is after the testing

---
+++ CMakeFiles/hw0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw0.dir/main.o"
+  "hw0.pdb"
+  "hw0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
